f8d6dc6489d767e33dc70d02479ffa91aab18b96
Taha Saif <160765440+Tahasaif3@users.noreply.github.com>
2024-06-03 00:45:54 +0500

    Add files via upload

---
+++ Loops Assignment 3/main.js
@@ -0,0 +1,6 @@
+var counter = 0;
+var step = 2;
+do {
+    console.log("Counter:", counter);
+    counter += step;
+} while (counter < 100);
---
+++ Loops Assignment 3/main.ts
@@ -0,0 +1,31 @@
+// 3. Counter Incrementer (Using do while loop )
+// Objective: Create a program that increments a counter by a specified step value using a
+// do...while loop and prints the counter value to the console until it reaches or exceeds 100.
+// Steps to Follow:
+// 1. Set the starting counter to 0: Create a variable counter and initialize it to 0.
+// 2. Create a variable, step, to increase your counter by: Define a variable step to hold
+// the value by which the counter will be incremented.
+// 3. Add a do...while loop: In the loop, print the counter to the console and increment it by
+// the step amount each iteration.
+// 4. Continue to loop until the counter is equal to or more than 100: The loop should run
+// as long as the counter is less than 100.
+
+// Step 1: Set the starting counter to "0"
+let counter = 0;
+
+//Step 2: Create a varable, step, to increase your counter by 
+let step = 2;
+
+// Step 3: Add do...while loop
+//
+do {
+   // Print the counter to the console
+    console.log("Counter:",counter);
+
+   // Increment the counter by the step amount
+   counter += step;
+
+// 4. Continue to loop until the counter is equal to or more than 100
+} while (counter < 100);
+
+
